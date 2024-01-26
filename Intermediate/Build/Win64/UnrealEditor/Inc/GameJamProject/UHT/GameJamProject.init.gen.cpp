1a5250216e958fd61c2ad73959c9747213207c42
Bogdan Panow <GoggiProsto@gmail.com>
2024-01-27 00:43:36 +0100

    Comedian

--- Intermediate/Build/Win64/UnrealEditor/Inc/GameJamProject/UHT/GameJamProject.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/GameJamProject/UHT/GameJamProject.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeGameJamProject_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x431B193C,
-				0xE0C5687E,
+				0x70EC36FC,
+				0xBCADF910,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_GameJamProject.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_GameJamProject.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_GameJamProject(Z_Construct_UPackage__Script_GameJamProject, TEXT("/Script/GameJamProject"), Z_Registration_Info_UPackage__Script_GameJamProject, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x431B193C, 0xE0C5687E));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_GameJamProject(Z_Construct_UPackage__Script_GameJamProject, TEXT("/Script/GameJamProject"), Z_Registration_Info_UPackage__Script_GameJamProject, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x70EC36FC, 0xBCADF910));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/GameJamProject/Characters/StanduperCharacter.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "StanduperCharacter.generated.h"
+
+UCLASS()
+class GAMEJAMPROJECT_API AStanduperCharacter : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AStanduperCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* SkeletalMesh;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
